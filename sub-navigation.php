948bd5f5d9bf2b9ebf09c61a4eb873a18a9d18da
Andi North <andi@mangopear.co.uk>
2018-01-21 13:28:32 +0000

    [Global] Header: Fixed broken link in sub navigation. Fixes #3

--- sub-navigation.php
+++ sub-navigation.php
@@ -3,7 +3,7 @@
 		<ul class="o-nav__list">
 			<li class="o-nav__item"><a href="/" class="o-nav__link">Home</a></li>
 			<li class="o-nav__item"><a href="/routes/" class="o-nav__link">Routes</a></li>
-			<li class="o-nav__item"><a href="/routes/destinations/" class="o-nav__link">Destinations</a></li>
+			<li class="o-nav__item"><a href="/destinations/" class="o-nav__link">Destinations</a></li>
 			<li class="o-nav__item"><a href="/blog/" class="o-nav__link">News &amp; Blog</a></li>
 		</ul>
 	</nav>
