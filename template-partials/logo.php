7a5106d3ee3e5f977a36c76669c1097b1dedfec7
Mangopear creative <andi@mangopear.co.uk>
2018-08-30 20:52:36 +0100

    [Global] Header: Added Scenic logo to header bar

---
+++ template-partials/logo.php
@@ -0,0 +1,4 @@
+<a href="<?php echo get_site_url(); ?>" class="c-main-header__button  c-main-header__button--left  o-button  o-button--just-icon  c-main-header__cell  c-main-header__cell--left">
+	<svg class="o-icon  o-icon--logo" height="40" width="131" role="presentation"><use xlink:href="<?php echo SCENIC_SPRITE; ?>#scenic-logo"/></svg>
+	<span class="u-invisible">Scenic</span>
+</a>
