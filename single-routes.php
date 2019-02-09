5b71fe6c158cc1563e06b2590665e1cd442fc3da
Andi North <andi@mangopear.co.uk>
2019-02-09 08:48:56 +0000

    [Routes] Single: Added places to template

--- single-routes.php
+++ single-routes.php
@@ -80,6 +80,61 @@
 				<?php endif; ?>
 			</div><!-- /.o-container -->
 		</section>
+
+
+
+
+
+		<?php if (have_rows('places')) : ?>
+			<section class="c-scenic-panel  c-scenic-panel--places  c-places">
+				<header class="c-scenic-panel__header">
+					<div class="o-container">
+						<h2 class="c-scenic-panel__title">Places to visit &amp; see along the route</h2>
+					</div><!-- /.o-container -->
+				</header>
+
+
+				<div class="c-places__list">
+					<div class="o-container">
+						<div class="o-grid">
+							<?php while (have_rows('places')) : the_row(); ?>
+								<div class="o-grid__item  u-one-third  u-lap--one-half  u-palm--one-whole">
+									<?php $place_type = get_sub_field('choice'); ?>
+
+
+									<?php if ($place_type == 'load') : ?>
+										<?php $location_term = get_term(get_sub_field('location'), 'route__locations'); ?>
+										<?php $place_image = get_field('image', $location_term); ?>
+
+										<article class="c-article  c-places__place">
+											<header class="c-article__header">
+												<img class="c-article__image" alt="<?php echo $location_term->name; ?>" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="<?php echo $place_image['sizes']['blog-lister']; ?>">
+												<h3 class="c-article__title"><a href="<?php echo get_term_link($location_term); ?>" class="c-article__title__link"><?php echo $location_term->name; ?></a></h3>
+											</header>
+
+											<div class="c-article__content"><?php the_field('description', $location_term); ?></div>
+										</article>
+
+
+									<?php else : ?>
+										<?php $place_image = get_sub_field('image'); ?>
+
+										<article class="c-article  c-places__place">
+											<header class="c-article__header">
+												<img class="c-article__image" alt="<?php the_sub_field('title'); ?>" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="<?php echo $place_image['sizes']['blog-lister']; ?>">
+												<h3 class="c-article__title"><?php the_sub_field('title'); ?></h3>
+											</header>
+
+											<div class="c-article__content"><?php the_sub_field('description'); ?></div>
+										</article>
+									<?php endif; ?>
+								</div><!-- /.o-grid__item -->
+							<?php endwhile; ?>
+						</div><!-- /.o-grid -->
+					</div><!-- /.o-container -->
+				</div><!-- /.c-places__list -->
+			</section><!-- /.c-places -->
+		<?php endif; ?>
 	</main><!-- /.o-panel -->
 
 
