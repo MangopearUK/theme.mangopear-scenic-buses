98176680640fcb838172fab55d010a936a7dbc8c
Andi North <andi@mangopear.co.uk>
2019-04-22 14:58:18 +0100

    [Reviews] Added function to update average review meta for route on comment publish and on status change of comment (i.e. trashed)

--- functions.php
+++ functions.php
@@ -33,6 +33,7 @@
 	 * [13]	Add definition for our SVG sprite document
 	 * [14]	Define search bar string
 	 * [15]	Disable ACF styles on front end
+	 * [16]	Calculate average rating for route, store as meta
 	 */
 	
 
@@ -248,4 +249,74 @@
 		wp_deregister_style('acf-field-group');
 		wp_deregister_style('acf-global');
 		wp_deregister_style('acf-input');
+
+
+
+
+
+	/**
+	 * [16]	Calculate average rating for route, store as meta
+	 *
+	 * 		@since 4.0.0
+	 *
+	 * 		[a]	Hook into comment creation action - this is the stage the acf 
+	 * 			fields are populated (not wp_insert_comment)
+	 * 		[b]	Hook into comment status change (i.e. if comment is trashed)
+	 *
+	 * 		[c]	Pull in vars from action [a]
+	 * 		[d]	Turn comment_id into obj
+	 * 		[e]	Hook into our bespoke function
+	 *
+	 * 		[f]	Pull in vars from action [b]
+	 * 		[d]	Turn comment_id into obj
+	 * 		[h]	Hook into our bespoke function
+	 *
+	 * 		[j]	Set empty array, to be populated later
+	 * 		[k]	Get comments relating to post ID of route
+	 * 		[l]	Loop through comments in [b]
+	 * 		[m]	Get the rating values
+	 * 		[n]	Push the rating value (int) to [a] array
+	 * 		[o]	Filter review array to remove empty values
+	 * 		[p]	Calculate review average
+	 * 		[r]	Round average to two decimal places
+	 * 		[s]	Delete previous post meta entry
+	 * 		[t]	Save as post meta
+	 */
+	
+	add_action('comment_post', 	        'scenic_update_rating_average_on_publish',       99, 3);			// [a]
+	add_action('wp_set_comment_status', 'scenic_update_rating_average_on_status_change', 99, 2);			// [b]
+
+
+	function scenic_update_rating_average_on_publish($comment_id, $comment_approved, $comment_data) {		// [c]
+		$comment_obj = get_comment($comment_id);															// [d]
+		scenic_calculate_rating($comment_obj);																// [e]
+	}																										// [c]
+
+
+	function scenic_update_rating_average_on_status_change($comment_id, $comment_status) {					// [f]
+		$comment_obj = get_comment($comment_id);															// [g]
+		scenic_calculate_rating($comment_obj);																// [h]
+	}																										// [f]
+
+
+	function scenic_calculate_rating($comment_obj) {														// [i]
+		$route_id = $comment_obj->comment_post_ID;															// [j]
+		$all_reviews = array();																				// [k]
+		$post_reviews = get_comments(array('post_id' => $route_id));										// [l]
+
+
+		foreach ($post_reviews as $review) {																// [m]
+			$review_rating = get_field('comments__rating', $review);										// [n]
+			$all_reviews[] = $review_rating['value'];														// [o]
+		}																									// [m]
+
+
+		$all_reviews = array_filter($all_reviews, function($x) { return $x !== ''; });						// [p]
+		$average_rating = array_sum($all_reviews) / count($all_reviews);									// [q]
+		$average_rating = number_format((float)$average_rating, 1, '.', '');								// [r]
+
+
+		delete_post_meta($route_id, 'scenic_review_rating');												// [s]
+		update_post_meta($route_id, 'scenic_review_rating', $average_rating);								// [t]
+	}																										// [i]
 	}
