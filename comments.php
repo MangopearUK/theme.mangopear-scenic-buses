ccfe46e33c4a6202ec88a24b9de5aaebaebe57a8
Andi North <andi@mangopear.co.uk>
2019-04-26 08:55:58 +0100

    [Reviews] Fixed issue with () appearing for reviews total when there were none.

--- comments.php
+++ comments.php
@@ -40,7 +40,7 @@
 					<?php comments_number('No reviews', '1 review', '% reviews'); ?>
 
 
-					<?php if (get_post_meta(get_the_ID(), 'scenic_review_rating', 1) != 'nan') : ?>
+					<?php if (get_post_meta(get_the_ID(), 'scenic_review_rating', 1) AND get_post_meta(get_the_ID(), 'scenic_review_rating', 1) != 'nan') : ?>
 						<span class="c-comments__header__ratings">
 							<?php scenic_output_rating_stars(get_the_ID()); ?>
 							<span class="c-comments__header__ratings__total">(<?php echo get_post_meta(get_the_ID(), 'scenic_review_rating', 1); ?>)</span>
