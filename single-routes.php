728e4faf0345c5feafcdaffbb8cb0f50ac3065ca
Andi North <andi@mangopear.co.uk>
2019-04-28 13:13:29 +0100

    [Route] Title: Added link to operator name to link to archive

--- single-routes.php
+++ single-routes.php
@@ -25,7 +25,7 @@
 
 				<h1 class="c-title__title">
 					<?php echo $route_name_number; ?>
-					<span class="c-title__title__sub"> from <?php $operator = get_field('operator'); echo $operator->name; ?></span>
+					<span class="c-title__title__sub"> from <?php $operator = get_field('operator'); ?> <a href="<?php echo get_term_link($operator); ?>"><?php echo $operator->name; ?></a></span>
 				</h1>
 
 
