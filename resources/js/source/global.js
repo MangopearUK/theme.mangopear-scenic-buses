eb51eb3b7c60b433ae116d7ab8198c0deb1b4e7c
Andi North <andi@mangopear.co.uk>
2020-07-19 20:21:57 +0100

    [Bug] Removed smooth scrolling JS

--- resources/js/source/global.js
+++ resources/js/source/global.js
@@ -34,29 +34,6 @@ jQuery(document).ready(function($){
 
 
 	/**
-	 * [2]	Smoothly scroll to anchors
-	 */
-	
-	$('a[href*="#"]:not([href="#"]), .js-smooth-scroll').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			
-
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
-
-
-
-
-
-	/**
 	 * [3]	Tickets & route carousels
 	 *
 	 * 		[a]	Set LazyLoadXT to load images outside of viewport
